c9371831f602b01544dd7536853b6837f77ec5d6
Bednar8 <p.bednarski9907@gmail.com>
2024-05-20 20:27:33 +0200

    fixed mobile menu

--- src/components/MenuMobile.jsx
+++ src/components/MenuMobile.jsx
@@ -1,18 +1,48 @@
 import {Link} from "react-router-dom"
 import {categories} from "../config"
+import {useMenuMobile} from "../context/MenuMobileContext"
+import {useEffect} from "react"
 
 // import earphonesIcon from "../assets/shared/desktop/image-category-thumbnail-earphones.png"
 // import headphonesIcon from "../assets/shared/desktop/image-category-thumbnail-headphones.png"
 // import speakersIcon from "../assets/shared/desktop/image-category-thumbnail-speakers.png"
 
 function MenuMobile() {
+	const {isMenuOpen} = useMenuMobile()
+
+	useEffect(() => {
+		if (isMenuOpen) {
+			document.body.style.overflow = "hidden"
+		} else {
+			document.body.style.overflow = "auto"
+		}
+
+		return () => {
+			document.body.style.overflow = "auto"
+		}
+	}, [isMenuOpen])
+
 	return (
-		<div className="absolute top-[100px] z-30 w-full bg-white h-max p-7 overflow-y-scroll">
-			<ul>
+		<div
+			className={`${
+				isMenuOpen && "left-[0]"
+			} absolute left-[-120%] top-[100px] z-30 w-full bg-white h-max p-7 transition-all overflow-y-auto`}>
+			<ul className="flex flex-col items-center justify-center">
 				{categories.map((categorie, i) => (
-					<li key={i}>
-						<img src={categorie.imageUrl} alt="" />
-						<Link to={`/categorie/${categorie.name}`}>{categorie.name}</Link>
+					<li key={i} className="flex items-center justify-center w-full ">
+						<div className="relative flex flex-col items-center justify-center text-black-secondary h-[150px] my-6 bg-gray-main  w-full z-0">
+							<img
+								src={categorie.imageUrl}
+								alt=""
+								className="w-[35%] top-[-60%] translate-y-[50%] absolute z-10"
+							/>
+							<p className="font-bold uppercase text-black-secondary">
+								{categorie.name}
+							</p>
+							<Link to={`/categorie/${categorie.name}`} className="uppercase">
+								shop
+							</Link>
+						</div>
 					</li>
 				))}
 			</ul>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -2,20 +2,13 @@ import {Outlet} from "react-router-dom"
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import {useMenuMobile} from "../context/MenuMobileContext"
-import MenuMobile from "../components/MenuMobile"
 
 function Layout() {
 	const {isMenuOpen} = useMenuMobile()
 
 	return (
 		<>
-			{isMenuOpen && <MenuMobile />}
-			<div
-				className={
-					isMenuOpen
-						? "overflow-hidden w-dvw h-dvh"
-						: "overflow-y-scroll w-dvw h-dvh"
-				}>
+			<div>
 				{isMenuOpen && (
 					<div className="fixed inset-0 w-dvw h-dvh bg-[#00000558] z-10"></div>
 				)}
